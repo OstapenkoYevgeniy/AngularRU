f13cd446428afba2e09b6202671bffd669bc0b27
Yevgeniy Ostapenko <OstapenkoYevgeniy@gmail.com>
2017-10-19 12:35:04 +0600

    Two commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,15 +5,22 @@ import {FormsModule} from "@angular/forms";
 import {AppComponent} from './app.component';
 
 @NgModule({
+    // классы представлений (view classes), которые принадлежат модулю.
+    // Angular имеет три типа классов представлений: компоненты (components), директивы (directives), каналы (pipes)
     declarations: [
         AppComponent
     ],
+    // imports: другие модули, классы которых необходимы для шаблонов компонентов из текущего модуля
     imports: [
         BrowserModule,
         FormsModule
     ],
+    // providers: классы, создающие сервисы, используемые модулем
     providers: [],
-    bootstrap: [AppComponent]
+    // bootstrap: корневой компонент, который вызывается по умолчанию при загрузке приложения
+    bootstrap: [AppComponent],
+    // набор классов представлений, которые должны использоваться в шаблонах компонентов из других модулей
+    exports: []
 })
 export class AppModule {
 }
